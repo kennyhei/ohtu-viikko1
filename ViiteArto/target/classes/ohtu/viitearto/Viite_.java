8d11508195f5fe6cdcfd737077a4877e68610996
Kenny Heinonen <kenny_heinonen@hotmail.com>
2012-04-04 20:28:55 +0300

    testi

---
+++ ViiteArto/target/classes/ohtu/viitearto/Viite_.java
@@ -0,0 +1,23 @@
+package ohtu.viitearto;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.2.0.v20110202-r8913", date="2012-04-04T20:07:44")
+@StaticMetamodel(Viite.class)
+public class Viite_ { 
+
+    public static volatile SingularAttribute<Viite, String> journal;
+    public static volatile SingularAttribute<Viite, Integer> yearMade;
+    public static volatile SingularAttribute<Viite, Long> id;
+    public static volatile SingularAttribute<Viite, String> author;
+    public static volatile SingularAttribute<Viite, String> title;
+    public static volatile SingularAttribute<Viite, String> address;
+    public static volatile SingularAttribute<Viite, String> pages;
+    public static volatile SingularAttribute<Viite, Integer> volume;
+    public static volatile SingularAttribute<Viite, String> booktitle;
+    public static volatile SingularAttribute<Viite, Integer> number;
+    public static volatile SingularAttribute<Viite, String> publisher;
+
+}
